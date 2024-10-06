1443813597b1cd47b7a2d906be3383f2b88fdde3
Elijah Johnson <ed.j53190@gmail.com>
2024-10-06 13:25:02 -0400

    Added Milestone 1 Source Files

---
+++ Milestone_1/java_src/Program1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Program1{
+    public record Result(int numPlatforms, int totalHeight, int[] numPaintings) {}
+   
+    /**
+    * Solution to program 1
+    * @param n number of paintings
+    * @param w width of the platform
+    * @param heights array of heights of the paintings
+    * @param widths array of widths of the paintings
+    * @return Result object containing the number of platforms, total height of the paintings and the number of paintings on each platform
+    */
+    private static Result program1(int n, int w, int[] heights, int[] widths) {
+        /************************
+         * ADD YOUR CODE HERE *
+         ***********************/
+
+        return new Result(0, 0, new int[0]);//replace with your own result
+        
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int W = sc.nextInt();
+        int[] heights = new int[n];
+        int[] widths = new int[n];
+        for(int i=0; i<n; i++){
+            heights[i] = sc.nextInt();
+        }
+        for(int i=0; i<n; i++){
+            widths[i] = sc.nextInt();
+        }
+        sc.close();
+        Result result = program1(n, W, heights, widths);
+        System.out.println(result.numPlatforms);
+        System.out.println(result.totalHeight);
+        for(int i=0; i<result.numPaintings.length; i++){
+            System.out.println(result.numPaintings[i]);
+        }
+    }
+}
