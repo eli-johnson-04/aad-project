f621d4ed0ef728a3f740692f5b74ba00cbc805b9
Elijah Johnson <ed.j53190@gmail.com>
2024-11-08 15:49:22 -0500

    Attempt?

--- Milestone_2/py_src/program3.py
+++ Milestone_2/py_src/program3.py
@@ -1,10 +1,11 @@
 from typing import List, Tuple
+import sys
 
 def calc_C(first: int, last: int):
     return 0
 
     
-def program3(n: int, W: int, heights: List[int], widths: List[int]) -> Tuple[int, int, List[int]]:
+def program3(n: int, W: int, heights: List[int], widths: List[int], C: List[List[int]]) -> Tuple[int, int, List[int]]:
     """
     Solution to Program 3
     
@@ -20,15 +21,23 @@ def program3(n: int, W: int, heights: List[int], widths: List[int]) -> Tuple[int
     List[int]: number of paintings on each platform
     """
     ############################
-    if n < 1:
+    if n < 1 or W == 0:
         return 0, 0, []
-    return min(
-        
-    )
+    
+    best_i = -1
+    min_C = sys.maxsize
+    for i in range(n - 2):
+        if C[i][n - 1] < min_C:
+            best_i = i
+            min_C = C[i][n - 1]
+    
+    return (min_C + program3(best_i - 1, W, heights, widths, C)[0], 0, [])
+
+
     ############################
 
 
-    return 0, 0, [] # replace with your code
+    #return 0, 0, [] # replace with your code
 
 
 if __name__ == '__main__':
@@ -36,7 +45,13 @@ if __name__ == '__main__':
     heights = list(map(int, input().split()))
     widths = list(map(int, input().split()))
 
-    m, total_height, num_paintings = program3(n, W, heights, widths)
+    c = [[-1] * n for _ in range (n)]
+    for i in range(n - 1, 0, -1):
+        for j in range(0, i - 1):
+            if sum(widths[j:i]) <= W:
+                c[i][j] = max(heights[j:i])
+
+    m, total_height, num_paintings = program3(n, W, heights, widths, c)
 
     print(m)
     print(total_height)
