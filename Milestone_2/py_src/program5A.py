02b13f827046a15ef84aade6ee70a5226fd6d5f8
PatrickKallenbach <pkallenbach1@ufl.edu>
2024-11-12 09:03:47 -0500

    import algo 3 in python as base for algo 5 development

--- Milestone_2/py_src/program5A.py
+++ Milestone_2/py_src/program5A.py
@@ -1,7 +1,9 @@
 from typing import List, Tuple
+import sys
+WIDTH_EXCEEDED = sys.maxsize
 
     
-def program5A(n: int, W: int, heights: List[int], widths: List[int]) -> Tuple[int, int, List[int]]:
+def program5A(n: int, W: int, heights: List[int], widths: List[int], C: List[int]) -> Tuple[int, int, List[int]]:
     """
     Solution to Program 5A
     
@@ -16,11 +18,55 @@ def program5A(n: int, W: int, heights: List[int], widths: List[int]) -> Tuple[in
     int: optimal total height
     List[int]: number of paintings on each platform
     """
-    ############################
-    # Add you code here
-    ############################
+    # Check for invalid input.
+    
+    if n < 1:
+        return (0, 0, [])
 
-    return 0, 0, [] # replace with your code
+    # Base case.
+    elif n == 1:
+        return (1, heights[0], [1])
+    
+    # Create a list of all possible return options over i, for 0 <= i <= n - 1.
+    
+    options = []
+    c_n = C[n - 1]
+    for i in range(n):
+        # Get the value of C for the current i and n values.
+
+        c_val = c_n[i]
+        
+        # Only include values for which the width is valid.
+        if c_val == WIDTH_EXCEEDED:
+            continue
+
+        # Use the remaining heights and widths.
+        tmpHeights = heights[:i]
+        tmpWidths = widths[:i]
+
+        # Get the return value.
+        ret = program5A(i, W, tmpHeights, tmpWidths, C)
+
+        # Only add a row if we are not in the n < 1 case.
+        rows = (n >= 1) + ret[0]
+        
+        # Calculate the cost of the now-constructed row.
+        cost = c_val + ret[1]
+        
+        # Create a temporary list to be used in the tuple at the front, and add the length of its row.
+        row_lengths = ret[2]
+        
+        # Bounds check.
+        if n >= 1:
+            # Insert length of row.
+            row_lengths.append(n - i)
+
+            # Add the tuple to the list.
+            options.append((rows, cost, row_lengths))
+
+    # Construct final return value.
+    result = min(options, key=lambda x: x[1])
+    return result
 
 
 if __name__ == '__main__':
@@ -28,7 +74,27 @@ if __name__ == '__main__':
     heights = list(map(int, input().split()))
     widths = list(map(int, input().split()))
 
-    m, total_height, num_paintings = program5A(n, W, heights, widths)
+    # Create a new vector for all C_ij values.
+    c = [[] for _ in range(n) ]
+
+    '''
+    Iterate over all c_ij values to determine the height of the tallest sculpture in all possible rows of width W. 
+    This list accessed with the n'th sculpture used as the first index, and the i value used as the second. 
+    '''
+    for j in range(n, 0, -1):
+        for i in range(j):
+            tmpWidths = widths[i:j]
+            ij_width = sum(tmpWidths)
+            
+            # Width check.
+            if ij_width <= W:
+                c[j - 1].append(max(heights[i:j]))
+            else:
+                c[j - 1].append(WIDTH_EXCEEDED)
+
+
+    m, total_height, num_paintings = program5A(n, W, heights, widths, c)
+
 
     print(m)
     print(total_height)
