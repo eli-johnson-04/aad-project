b9b255178510684c2d7bfe5169ab439d1b21f474
Nicholas L <66135546+NicholasLindner@users.noreply.github.com>
2024-11-13 13:30:26 -0500

    Update program4.py

--- Milestone_2/py_src/program4.py
+++ Milestone_2/py_src/program4.py
@@ -1,76 +1,64 @@
 from typing import List, Tuple
 import sys
 
-def program4(n: int, W: int, heights: List[int], widths: List[int]) -> Tuple[int, int, List[int]]:
-    '''
-    Solution to Program 4
+WIDTH_EXCEEDED = sys.maxsize
 
+
+def program4(n: int, heights: List[int], widths: List[int], W: int) -> Tuple[int, int, List[int]]:
+    """
     Parameters:
     n (int): number of paintings
-    W (int): maximum row width
     heights (List[int]): heights of the paintings
     widths (List[int]): widths of the paintings
+    W (int): maximum width of the platform
 
-    Returns: 
+    Returns:
     int: number of platforms used
     int: optimal total height
     List[int]: number of paintings on each platform
-    '''
-    # Find the cost of the row from painting_start to painting_end.
-    def row_cost(start, end):
-        max_h = 0  # max height
+    """
 
-        # Iterate over all paintings in the row.
-        for p in range(start, end + 1):
-            max_h = heights[p - 1] if (heights[p - 1] > max_h) else max_h
-            
-        return max_h
-    
-    # Set up dynamic programming array of size n + 1. 
-    infinity = sys.maxsize
-    dp = [infinity] * (n + 1)
-    dp[0] = 0
+    # Precompute C[i][j] (the max height of placing paintings from i to j on a platform)
+    C = []
+    for i in range(n):
+        row = [WIDTH_EXCEEDED] * n
+        C.append(row)
+    for i in range(n):
+        for j in range(i, n):
+            t_w = sum(widths[i:j + 1])
+            m_h = max(heights[i:j + 1])
+            if t_w <= W:
+                C[i][j] = m_h
 
-    # For any x where painting x is the end of a row, p[x] contains that row's starting index. 
-    p = [0] * (n + 1)
+    # Initialize the dytnamic programming array M as well as backtracking array P
+    M = [WIDTH_EXCEEDED] * (n + 1)
+    P = [-1] * (n + 1)
+    M[0] = 0
 
-    # Iterate over all paintings 1 to n inclusive. 
-    for j in range(1, n + 1):
-        total_w = 0 # combined with of all paintings in a row
+    # Calc min cost for each position using prev computed values
+    for i in range(1, n + 1):
+        cuts = []
+        for j in range(i):
+            c_val = C[j][i - 1]
+            if c_val != WIDTH_EXCEEDED:
+                cost = M[j] + c_val
+                cuts.append((cost, j))
 
-        # Beginning at cur, iterate backward to zero to check all possible rows ending at j.
-        for i in range(j, 0, -1):
-            # Add width of current painting to total width.
-            total_w += widths[i - 1]
+        M[i], P[i] = min(cuts, key=lambda x: x[0])
 
-            # Check row validity. 
-            if total_w > W:
-                break
+    # Reconstruct the platform solution using backtracking with P array
+    row_lens = []
+    i = n
+    while i > 0:
+        j = P[i]
+        row_lens.append(i - j)
+        i = j
+    row_lens.reverse()
 
-            # Calculate the cost of row starting at i and ending at j. 
-            max_h = row_cost(i, j)
-
-            # The total cost is the sum of the optimal cost of the newly constructed row and of the preceding row, which ends at i - 1.
-            total_cost = dp[i - 1] + max_h
-
-            # Update dp[j] if the new height is better. 
-            if total_cost < dp[j]:
-                dp[j] = total_cost
-                p[j] = i
-
-    # This list contains the length of all rows, and its length = the total number of rows. 
-    row_lengths = []
-    
-    # Backtrack to find the number of paintings on each platform. 
-    j = n
-    while j > 0:
-        start = p[j]
-        row_lengths.insert(0, j - start + 1)
-        
-        # Move to the preceding row. 
-        j = start - 1
-
-    return len(row_lengths), dp[n], row_lengths
+    # Calculate the total number of platforms and the optimal total cost
+    total_platforms = len(row_lens)
+    total_cost = M[n]
+    return total_platforms, total_cost, row_lens
 
 
 if __name__ == '__main__':
